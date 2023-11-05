6091a34b286a7c8c640c609c033327348cb3402b
Mustafa Uysal <uysall.mustafaa@gmail.com>
2023-11-05 23:54:54 +0300

    added

---
+++ lib/widgets/widgets_constants.dart
@@ -0,0 +1,40 @@
+class WidgetConstants {
+  //Border Radius
+  static const double borderRadius1 = 38.0;
+  static const double borderRadius2 = 30.0;
+  static const double borderRadius3 = 15.0;
+  static const double borderRadius4 = 12.0;
+  static const double borderRadius5 = 8.0;
+  static const double borderRadius6 = 5.0;
+
+  //Button Height
+  static const double buttonHeightHigh = 48;
+  static const double buttonHeightMedium = 40.0;
+  static const double buttonHeightLow = 30.0;
+
+  //Button Width
+  static const double buttonWidthHigh = 350;
+  static const double buttonWidthMedium = 200;
+  static const double buttonWidthLow = 100;
+
+  //Elevation
+  static const double elevation1 = 20.0;
+  static const double elevation2 = 10.0;
+  static const double elevation3 = 6.0;
+  static const double elevation4 = 4.0;
+  static const double elevation5 = 1.0;
+
+  //Padding
+  static const double padding1 = 20.0;
+  static const double padding2 = 16.0;
+  static const double padding3 = 12.0;
+  static const double padding4 = 8.0;
+  static const double padding5 = 5.0;
+  static const double padding6 = 3.0;
+  static const double padding7 = 2.0;
+
+  //IconSizes
+  static const double iconSizeSmall = 20;
+  static const double iconSizeMedium = 30;
+  static const double iconSizeLarge = 40;
+}
